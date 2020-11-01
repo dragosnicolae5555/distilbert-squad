37eef987ebbb21375e16fd0048772d01d915352c
Constantin Nicolae <dragosnicolae555@gmail.com>
2020-11-01 19:37:48 +0100

    Update app.py
    add webhook

--- app.py
+++ app.py
@@ -1,8 +1,12 @@
 from flask import request
 from model import model
 import flask
-
-
+import numpy as np
+import os
+import time
+import json
+from flask import request, make_response
+from flask_cors import cross_origin
 app = flask.Flask(__name__)
 
 
@@ -20,6 +24,47 @@ def index():
         return flask.render_template('index.html', question=question, answer=answer["answer"])
     else:
         return flask.render_template('index.html')
+    
+
+
+# geting and sending response to dialogflow
+@app.route('/webhook', methods=['POST'])
+@cross_origin()
+
+def webhook():
+
+    req = request.get_json(silent=True, force=True)
+
+    #print("Request:")
+    #print(json.dumps(req, indent=4))
+
+    res = processRequest(req)
+
+    res = json.dumps(res, indent=4)
+    #print(res)
+    r = make_response(res)
+    r.headers['Content-Type'] = 'application/json'
+    return r
+
+
+# processing the request from dialogflow
+def processRequest(req):
+    #sessionID=req.get('responseId')
+    result = req.get("queryResult")
+    #user_says=result.get("queryText")
+    #log.write_log(sessionID, "User Says: "+user_says)
+    parameters = result.get("parameters")
+    context=parameters.get("context")
+    question = parameters.get("question")
+
+    intent = result.get("intent").get('displayName')
+    
+    if (intent=='test1'):
+        answer =model.predict(context, question)
+    fulfillmentText = "{} !".format(answer['answer'])
+    return {
+            "fulfillmentText": fulfillmentText
+        }
 
 
 if __name__ == "__main__":
